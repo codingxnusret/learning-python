546f294ae135c8dac3d00dd3ed143ba0d22061a6
codingxnusret <nusretthecoder@gmail.com>
2019-06-30 10:38:07 -0600

    I learned the different variable types and how to create them

---
+++ LearningVariables.py
@@ -0,0 +1,21 @@
+#Variables and Data Types review
+
+#1.create a variable called intVal and assign it an integer value
+intVal=5
+#2.create a variable called floatVal and assign it  a float value
+floatVal= 6.5
+#3 create a variable called boolVal and assign it a Boolean value
+boolVal=False
+#4 use a print() to display the Blooean value  assigned to boolVal in the output of the program
+print(boolVal)
+#5.reassign blooVal a different value than the one assigned to it in step 3
+boolVal=True
+#6.use print() to display the integer value assigned to inVal in the output of the program
+
+print(intVal)
+
+#7.use print() to displae the float value assigned to floatVal in the output of the program
+print(floatVal)
+#8.use print() to the boolean value reassigned to boolVal in the output of the program
+print(boolVal)
+
---
+++ firstfile.py
@@ -0,0 +1,3 @@
+print("hello")
+print("best")
+print("hey")
